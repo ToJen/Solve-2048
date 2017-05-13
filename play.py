d43fa14a26c8c22ed597f2caceb4af980a5d2e1d
Tomisin <tomisinjenrola@gmail.com>
2017-05-13 14:40:05 -0230

    Refactored code

--- play.py
+++ play.py
@@ -13,47 +13,42 @@ gameElem = driver.find_element_by_class_name('grid-container')
 directions = [Keys.UP, Keys.DOWN, Keys.RIGHT, Keys.LEFT]
 
 ActionChains(driver).move_to_element_with_offset(gameElem, 0, 50).perform()
-# print("moving...")
+
 time.sleep(3)
-# print("moved...")
-# choice_last = None
-# choice = None
-# while True:
-# 	for direction in directions:
-# 		choice = random.choice(direction)
-# 		# if choice == choice_last:
 
-# 		ActionChains(driver).send_keys(choice).perform()
-# 		# time.sleep(2)
-# 		# choice_last = choice
+# TODO: use some algorithm to decide which direction to move to
+# def choose_next_move():
 
 
-def isOver(browser):
+def print_current_tiles(curr_round):
+	tiles = driver.find_elements_by_class_name('tile')
+	for tile in tiles:
+		print(curr_round, ": ", tile.text, end=" ")
+
+def is_over(browser):
 	try:
 		game_over = browser.find_element_by_class_name('game-over')
-		print(type(game_over))
 		return game_over
 	except:
-		print("not over yet")
+		# print("not over yet")
+		pass
 
 
 game_ended = False
 i = 0
 while not game_ended:
-	game_ended = isOver(driver)
-	print("i: %s" % i)
+	game_ended = is_over(driver)
 	
 	if game_ended:
 		print('game over boi!')
-		time.sleep(3)
 		i += 1
 		game_ended = True if (i==2) else False
-		print('restarting...')
+		time.sleep(3)
 		restartElem = driver.find_element_by_link_text('Try again')
 		restartElem.click()
+		# print('restarting...')
 
 	for direction in directions:
 		choice = random.choice(direction)
+		# print_current_tiles(i)
 		ActionChains(driver).send_keys(choice).perform()
-
-
